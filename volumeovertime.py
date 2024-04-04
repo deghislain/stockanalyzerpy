a7d3d17e0ce6e5bc313bdc0a8f627373d2892ca3
armel <deghislain@gmail.com>
2024-04-04 11:29:48 -0300

    added stock symbol to the query

--- volumeovertime.py
+++ volumeovertime.py
@@ -26,8 +26,8 @@ def get_db_url():
     return PG_URL
 
 
-def get_stock_volume_by_time_period(PG_URL, period_start, period_end):
-    sql = "SELECT stock_volume, curr_stock_date FROM av_weekly_stock_data WHERE curr_stock_date BETWEEN \'" + period_start + "\' AND \'" + period_end +"\' order by curr_stock_date"
+def get_stock_volume_by_time_period(PG_URL, period_start, period_end, symbol):
+    sql = "SELECT stock_volume, curr_stock_date FROM av_weekly_stock_data WHERE curr_stock_date BETWEEN \'" + period_start + "\' AND \'" + period_end +"\' AND stock_symbol = \'" + symbol +"\' order by curr_stock_date"
     engine = db.create_engine(PG_URL, echo=True)
     df = pd.read_sql_query(sql, engine)
     return df
@@ -48,11 +48,10 @@ def get_str_stock_date_array(data):
 
 def main():
     db_url = get_db_url()
-    data = get_stock_volume_by_time_period(db_url, "2024-03-01", "2024-04-01")
+    data = get_stock_volume_by_time_period(db_url, "2024-03-01", "2024-04-01", "IBM")
     x = get_str_stock_date_array(data.curr_stock_date)
     y = get_str_stock_volume_array(data.stock_volume)
 
-    # x = get_current_stock_date_by_time_period(db_url,"2024-03-01", "2024-04-01")
     font1 = {'family': 'serif', 'color': 'blue', 'size': 20}
     font2 = {'family': 'serif', 'color': 'darkred', 'size': 15}
     plt.title("Stock Volume Data", fontdict=font1)
